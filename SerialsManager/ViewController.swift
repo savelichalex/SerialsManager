6dec38a93838011df9569bbe309b7359013b3c8c
savelichalex <savelichalex93@gmail.com>
2016-07-04 13:17:41 +0300

    Initial Commit

---
+++ SerialsManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SerialsManager
+//
+//  Created by Admin on 04.07.16.
+//  Copyright © 2016 savelichalex. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

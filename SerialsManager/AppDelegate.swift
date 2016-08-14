05825e6d4944f351574e3a0eeafaa7026a89637b
Alexey Savelev <savelichalex93@gmail.com>
2016-08-14 20:20:45 +0300

    Add config file

--- SerialsManager/AppDelegate.swift
+++ SerialsManager/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        if let path = NSBundle.mainBundle().pathForResource("AppConfig", ofType: "plist") {
+            let appConfig = NSDictionary(contentsOfFile: path)
+            print(appConfig)
+            // use config somehow
+        }
+        
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
